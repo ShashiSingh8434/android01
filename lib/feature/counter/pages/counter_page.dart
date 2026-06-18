289eedfa68170be7569e273eedb3e853234bea1c
Shashi Singh <alphasingh8434@gmail.com>
2026-06-18 18:30:57 +0530

    MVVM & feature first - architecture initialised

---
+++ lib/feature/counter/pages/counter_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Counter Page")),
+      body: Center(child: Text("Counter Page")),
+    );
+  }
+}
